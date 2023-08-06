2f7c75fa6f046e65045a93e29b9cd6ce46476b9b
Rathan00 <87562942+Rathan00@users.noreply.github.com>
2023-08-06 16:20:54 +0530

    Busses.java

--- Busses.java
+++ Busses.java
@@ -1,64 +1,40 @@
-
-public class Busses {
-	public static void main(String[] args) {
-		
-	} 
-}
 class Buses{
 	private int busId;
-	private String number;
-	private String departure;
-	private String destination;
+	private String busNumber;
+	private String travelAgency;
 	private String busType;
-	private long availableDate[];//ddmmyy format example 070723 is 7th jul 2023
-	private int rootId;
-	private int startTime;//24hr format hhmm example 1830 is 18hrs 30 min
-	private int availableSeats[];
-	private int journeyTime;//hhmm format example 0825 means 8hrs 25min
-	private int endTime;
-	private long reachingDate[];
-	public Buses(int busID, String number, String departure, String destination, String busType, long[] availableDate,
-			int rootId, int startTime, int[] availableSeats, int journeyTime, int endTime, long[] reachingDate) {
+	private int availableSeats;
+	private int totalSeats;
+	private float fare;
+	public Buses(int busId, String busNumber, String travelAgency, String busType, int availableSeats, int totalSeats,
+			float fare) {
 		super();
-		this.busId = busID;
-		this.number = number;
-		this.departure = departure;
-		this.destination = destination;
+		this.busId = busId;
+		this.busNumber = busNumber;
+		this.travelAgency = travelAgency;
 		this.busType = busType;
-		this.availableDate = availableDate;
-		this.rootId = rootId;
-		this.startTime = startTime;
 		this.availableSeats = availableSeats;
-		this.journeyTime = journeyTime;
-		this.endTime = endTime;
-		this.reachingDate = reachingDate;
+		this.totalSeats = totalSeats;
+		this.fare = fare;
 	}
-	public Buses() {
-		// TODO Auto-generated constructor stub
-	}
+	public Buses() {}
 	public int getBusId() {
 		return busId;
 	}
 	public void setBusId(int busId) {
 		this.busId = busId;
 	}
-	public String getNumber() {
-		return number;
+	public String getBusNumber() {
+		return busNumber;
 	}
-	public void setNumber(String number) {
-		this.number = number;
+	public void setBusNumber(String busNumber) {
+		this.busNumber = busNumber;
 	}
-	public String getDeparture() {
-		return departure;
+	public String getTravelAgency() {
+		return travelAgency;
 	}
-	public void setDeparture(String departure) {
-		this.departure = departure;
-	}
-	public String getDestination() {
-		return destination;
-	}
-	public void setDestination(String destination) {
-		this.destination = destination;
+	public void setTravelAgency(String travelAgency) {
+		this.travelAgency = travelAgency;
 	}
 	public String getBusType() {
 		return busType;
@@ -66,48 +42,22 @@ class Buses{
 	public void setBusType(String busType) {
 		this.busType = busType;
 	}
-	public long[] getAvailableDate() {
-		return availableDate;
-	}
-	public void setAvailableDate(long[] availableDate) {
-		this.availableDate = availableDate;
-	}
-	public int getRootId() {
-		return rootId;
-	}
-	public void setRootId(int rootId) {
-		this.rootId = rootId;
-	}
-	public int getStartTime() {
-		return startTime;
-	}
-	public void setStartTime(int startTime) {
-		this.startTime = startTime;
-	}
-	public int[] getAvailableSeats() {
+	public int getAvailableSeats() {
 		return availableSeats;
 	}
-	public void setAvailableSeats(int[] availableSeats) {
+	public void setAvailableSeats(int availableSeats) {
 		this.availableSeats = availableSeats;
 	}
-	
-	public int getJourneyTime() {
-		return journeyTime;
+	public int getTotalSeats() {
+		return totalSeats;
 	}
-	public void setJourneyTime(int journeyTime) {
-		this.journeyTime = journeyTime;
+	public void setTotalSeats(int totalSeats) {
+		this.totalSeats = totalSeats;
 	}
-	public int getEndTime() {
-		return endTime;
+	public float getFare() {
+		return fare;
 	}
-	public void setEndTime(int endTime) {
-		this.endTime = endTime;
+	public void setFare(float fare) {
+		this.fare = fare;
 	}
-	public long[] getReachingDate() {
-		return reachingDate;
-	}
-	public void setReachingDate(long[] reachingDate) {
-		this.reachingDate = reachingDate;
-	}
-	
 }
